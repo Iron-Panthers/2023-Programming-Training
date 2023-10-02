fe5c4dc79505f3098759fb7a4a36f11e57d95b7d
EvelynCoolMop <evelynhon17@gmail.com>
2023-10-02 16:39:08 -0700

    I copied samantha

--- frc2022-mecanum-drive-ex/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ frc2022-mecanum-drive-ex/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -15,12 +15,19 @@ public class DriveSubsystem extends SubsystemBase {
     private TalonSRX mRearLeftTalon;
     private TalonSRX mFrontRightTalon;
     private TalonSRX mRearRightTalon;
+    
 
     private double m_frontLeftCoeff = 1;
     private double m_rearLeftCoeff = 1;
     private double m_frontRightCoeff = 1;
     private double m_rearRightCoeff = 1;
+    private double FrontLeftWheel = 1;
+    private double FrontRightWheel = 1;
+    private double RearLeftWheel = 1;
+    private double RearRightWheel = 1;
 
+    // private int multiTH = 1; // wheels = t(left Front)   y(Right Front)
+    // private int multiYG = 1; //          g(left back)    h(Right back)
 
     private ControlMode m_driveControlMode = ControlMode.PercentOutput;
 
@@ -37,23 +44,65 @@ public class DriveSubsystem extends SubsystemBase {
     // movement, and Z axis for rotation.
         // mRobotDrive.driveCartesian(ySpeed, xSpeed, zRot, 0.0);
 
-        ySpeed.getAsDouble();
+        double y = ySpeed.getAsDouble();
+        double x = xSpeed.getAsDouble();
+// //IMININ
+// if (x>y)
+//         {
+//           multiTH = -1;
+//         }
+//         else
+//         {
+//           multiTH = 1;
+//         }
+//         if (y>(x*-1))
+//         {
+//           multiYG = -1;
+//         }
+//         else
+//         {
+//           multiYG = 1;
+//         }
+//         double speed1 = Math.abs((Math.sqrt((x*x)+(y*y)))/Math.sqrt(2)); // finding distance of joystick to center
+//         double speed2 = math.abs(0.5*(y + x)); // finding solution to split line equation x=y and joystick locaiton equation, y=-x+xSpeed+ySpeed
+//         double speed = ((speed1*speed2));
 
-        mFrontLeftTalon.set(m_driveControlMode, ySpeed.getAsDouble());
-        mFrontRightTalon.set(m_driveControlMode, ySpeed.getAsDouble() + xSpeed.getAsDouble());
-        mRearLeftTalon.set(m_driveControlMode, ySpeed.getAsDouble());
-        mRearRightTalon.set(m_driveControlMode,ySpeed.getAsDouble() + xSpeed.getAsDouble());
 
+//         mFrontLeftTalon.set(m_driveControlMode, speed*multiTH);
+//         mFrontRightTalon.set(m_driveControlMode,speed*multiYG);
+//         mRearLeftTalon.set(m_driveControlMode, speed*multiYG);
+//         mRearRightTalon.set(m_driveControlMode,speed*multiTH);
+//IMIMNINI
+        
+        mFrontLeftTalon.set(m_driveControlMode, y + x);
+        mFrontRightTalon.set(m_driveControlMode, y + x);
+        mRearLeftTalon.set(m_driveControlMode, y + x);
+        mRearRightTalon.set(m_driveControlMode, y + x);
+
+        mFrontLeftTalon.set(m_driveControlMode, FrontLeftWheel);
+        mFrontRightTalon.set(m_driveControlMode, FrontRightWheel);
+        mRearLeftTalon.set(m_driveControlMode, RearLeftWheel);
+        mRearRightTalon.set(m_driveControlMode, RearRightWheel);
         
     }
-
-
-    
+      if (y>0){
+        FrontLeftWheel = 1;
+        FrontRightWheel = 1;
+        RearLeftWheel = 1;
+        RearRightWheel = 1;
+      }else if (y<0){
+        FrontLeftWheel = -1;
+        FrontRightWheel = -1;
+        RearLeftWheel = -1;
+        RearRightWheel = -1;
+      }
+  
       public void setMotorCoeff(
         double frontLeftCoeff,
         double rearLeftCoeff,
         double frontRightCoeff,
-        double rearRightCoeff) {
+        double rearRightCoeff,) {
+      
       m_frontLeftCoeff = frontLeftCoeff;
       m_rearLeftCoeff = rearLeftCoeff;
       m_frontRightCoeff = frontRightCoeff;
@@ -62,12 +111,13 @@ public class DriveSubsystem extends SubsystemBase {
 
       /**
    * Set control mode and velocity scale (opt)
-   * 
+    
    * @param controlMode control mode to use setting talon output
    * @param velocityScale velocity for full scale in ticks/100ms
    */
   public void setControlMode(ControlMode controlMode, double velocityScale) {
     m_driveControlMode = controlMode;
+    
   }
 }
 
